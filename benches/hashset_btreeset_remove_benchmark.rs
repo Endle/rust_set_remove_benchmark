15632171f13e9956542cce55cb1a4c97f0beabd8
Zhenbo Li <litimetal@gmail.com>
2021-07-26 21:01:30 +0800

    Finish test
    Signed-off-by: Zhenbo Li <litimetal@gmail.com>

--- benches/hashset_btreeset_remove_benchmark.rs
+++ benches/hashset_btreeset_remove_benchmark.rs
@@ -1,38 +1,54 @@
 use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+use rand::rngs::StdRng;
+use rand::{Rng,SeedableRng};
+use rand::prelude::SliceRandom;
+use std::collections::{HashSet, BTreeSet};
 
-fn fibonacci_slow(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci_slow(n-1) + fibonacci_slow(n-2),
+fn hashset_test(n: usize) -> () {
+    let (push, pop) = generate_random_operations(n);
+    let mut set = HashSet::with_capacity(n);
+    for x in push {
+        set.insert(x);
+    }
+    for y in pop {
+        set.remove(&y);
     }
 }
 
-fn fibonacci_fast(n: u64) -> u64 {
-    let mut a = 0;
-    let mut b = 1;
-
-    match n {
-        0 => b,
-        _ => {
-            for _ in 0..n {
-                let c = a + b;
-                a = b;
-                b = c;
-            }
-            b
-        }
+fn btreeset_test(n: usize) -> () {
+    let (push, pop) = generate_random_operations(n);
+    let mut set = BTreeSet::new();
+    for x in push {
+        set.insert(x);
+    }
+    for y in pop {
+        set.remove(&y);
     }
 }
 
+
+
+const RANDOM_RANGE:u32 = 1_000_000_000;
+fn generate_random_operations(n: usize) ->(Vec<u32>, Vec<u32>) {
+    let mut rng = StdRng::seed_from_u64(n as u64);
+    let mut input = Vec::with_capacity(n);
+    for _ in 0..n {
+        input.push(rng.gen_range(0..RANDOM_RANGE));
+    }
+    let mut pop = input.clone();
+    pop.shuffle(&mut rng);
+    (input, pop)
+}
 
 fn bench_set_pop(c: &mut Criterion) {
     let mut group = c.benchmark_group("Set_Remove");
-    for i in [20u64, 21u64].iter() {
-        group.bench_with_input(BenchmarkId::new("Recursive", i), i,
-                               |b, i| b.iter(|| fibonacci_slow(*i)));
-        group.bench_with_input(BenchmarkId::new("Iterative", i), i,
-                               |b, i| b.iter(|| fibonacci_fast(*i)));
+
+    for i in [10, 100, 500, 1000, 5000, 10000,
+        25_000, 50_000, 100_000, 200_000, 300_000, 400_000, 500_000].iter() {
+        group.bench_with_input(BenchmarkId::new("Hashset", i), i,
+                               |b, i| b.iter(|| hashset_test(*i)));
+        group.bench_with_input(BenchmarkId::new("BTree", i), i,
+                               |b, i| b.iter(|| btreeset_test(*i)));
     }
     group.finish();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
     println!("Hello, world!");
+    for _ in 0..5 {
+        let (push, pop) = generate_random_operations(10);
+        println!{"{:?}",&push};
+        println!{"{:?}", &pop};
+        println!("===========")
+    }
+}
+
+use rand::rngs::StdRng;
+use rand::{Rng,SeedableRng};
+use rand::prelude::SliceRandom;
+
+const RANDOM_RANGE:u32 = 1_000_000_000;
+fn generate_random_operations(n: usize) ->(Vec<u32>, Vec<u32>) {
+    let mut rng = StdRng::seed_from_u64(n as u64);
+    let mut input = Vec::with_capacity(n);
+    for _ in 0..n {
+        input.push(rng.gen_range(0..RANDOM_RANGE));
+    }
+    let mut pop = input.clone();
+    pop.shuffle(&mut rng);
+    (input, pop)
 }
